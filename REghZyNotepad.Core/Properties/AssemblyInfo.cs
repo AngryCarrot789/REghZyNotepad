0f6f00ce7905d5864ac969ca31a9741267592f3b
reghzy <kettlesimzulator420@gmail.com>
2021-08-15 16:46:53 +0100

    Added assembly info stuff

--- REghZyNotepad.Core/Properties/AssemblyInfo.cs
+++ REghZyNotepad.Core/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("REghZyNotepad.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core utilities for REghZyNotepad, including the viewmodels, services, etc")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("REghZY")]
 [assembly: AssemblyProduct("REghZyNotepad.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.30")]
+[assembly: AssemblyFileVersion("1.0.1.30")]
--- REghZyNotepad/Properties/AssemblyInfo.cs
+++ REghZyNotepad/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("REghZyNotepad")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A remake of windows notepad with a dark theme and more text editor features (no find feature yet though)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("REghZy")]
 [assembly: AssemblyProduct("REghZyNotepad")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.37")]
+[assembly: AssemblyFileVersion("1.0.2.37")]
